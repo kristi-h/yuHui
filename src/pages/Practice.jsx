f8041525053001b8934d7462184a8ec4a7dbf772
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-06-18 11:32:54 -0700

    initialize project

---
+++ src/pages/Practice.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Grid from '../components/Grid'
+
+export default function Practice(){
+    return(
+        <>
+            <h3> English Word</h3>
+            <h3> English Word</h3>
+            <Grid />
+        </>
+
+    )
+}
