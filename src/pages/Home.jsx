f9820b5d6da5bfadab49c8019590f7a15723d899
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-06-21 11:12:57 -0700

    export shuffle function and add prev & next buttons on Practice pg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,15 @@ import { useNavigate } from 'react-router-dom'
 import '../App.css'
 import { hsk3 } from '../hsk3.jsx'
 
-// export function shuffle(array) {
-//     let currentIndex = array.length;
-//     while (currentIndex != 0) {
-//       let randomIndex = Math.floor(Math.random() * currentIndex);
-//       currentIndex--;
-//       [array[currentIndex], array[randomIndex]] = [
-//         array[randomIndex], array[currentIndex]];
-//     } 
-// }
+export function shuffle(array) {
+    let currentIndex = array.length;
+    while (currentIndex != 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    } 
+}
 
 export default function Home(){
   const navigate = useNavigate()
@@ -23,6 +23,8 @@ export default function Home(){
 
   React.useEffect(()=>{
     function createClusters(){
+    // console.log(typeof vocabList) 
+    // console.log(Array.isArray(vocabList))
       shuffle(vocabList)
       const clustered = []
       for (let i=0; i<vocabList.length; i++){
@@ -40,16 +42,6 @@ export default function Home(){
     navigate(`/practice/:${e.target.value}`)
   }
   console.log('chosen-cluster', cluster)
-
-  function shuffle(array) {
-    let currentIndex = array.length;
-    while (currentIndex != 0) {
-      let randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    } 
-}
 
   function clustersEle(){
     console.log(clusters)
--- src/pages/Practice.jsx
+++ src/pages/Practice.jsx
@@ -15,8 +15,12 @@ export default function Practice({cluster, level}){
     //     shuffle(cluster)
     // }, [])
 
+    function getNextWord() {
+        console.log('next word')
+    }
+
     return(
-        <>
+        <div className='practice-container' >
             <h3> English Word </h3>
             <h3> Pinyin </h3>
             <div className='grid-container answer-container'>
@@ -25,7 +29,9 @@ export default function Practice({cluster, level}){
             </div>
             <br></br>
             <Grid level={level}/>
-        </>
+                <button className='btn prev-btn' onClick={getNextWord}>Previous</button>
+                <button className='btn next-btn' onClick={getNextWord}>Next</button>
+        </div>
 
     )
 }
