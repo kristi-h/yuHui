6fcb6153a582c6a64018405df7498729cb366b8c
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-07-07 21:56:54 -0700

    add second column to grid-container as default grid

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -7,64 +7,39 @@ import { useMemo, useState } from 'react';
 
 export default function Grid({level, cluster, handleClick, currentWord }){
     // let gridChar = useMemo(()=> getGrid(level, cluster), [level, currentWord])
-    let gridChar = cluster.slice(0, 2)
+    let gridChar = cluster.slice(0, 3)
+    console.log("currentWord", currentWord)
 
     function getGrid(){
         function setLevel(){
             console.log('level-cluster', cluster)
             if (level === "difficult"){
-                gridChar = cluster.slice(0, 14)
+                gridChar = cluster.slice(0, 15)
             } else if (level === "medium"){
-                gridChar = cluster.slice(0, 6)
+                gridChar = cluster.slice(0, 7)
             } else {
-                gridChar = cluster.slice(0, 2)
+                level ==="easy"
+                gridChar = cluster.slice(0, 3)
             }
         }
-        setLevel
+        setLevel()
         randomizeGridChar()
-    }  
+    } 
 
     function randomizeGridChar(){
         gridChar.push(currentWord)
         shuffle(gridChar)
     }
-//    React.useEffect(()=> {
-//     function setLevel(){
-//         console.log('level-cluster', cluster)
-//         if (level === "difficult"){
-//             setGridChar(cluster.slice(0, 14))
-//         } else if (level === "medium"){
-//             setGridChar(cluster.slice(0, 6))
-//         } else {
-//             setGridChar(cluster.slice(0, 2))
-//         }
-//     }
-//     setLevel()
-//    }, [level])
-//    console.log('gridChar', gridChar)
-//    console.log('grid-cluster', cluster)
-
-// React.useEffect(()=> {
-//     function randomizeGridChar(){
-//         setGridChar(gridChar, currentWord)
-//         shuffle(gridChar)
-//     }
-    
-//     randomizeGridChar()
-//    },[currentWord])
 
     console.log('gridChar', gridChar)
     const createGrid = ()=> (
         gridChar.map((char, index)=> (
-            // <>
-            //     <div key={index} >Testing</div>
-            //     <Link to='/'>Go back home</Link>
-            // </>
             <GridSquare key={index} handleClick={handleClick} char={char} />
     )))
     
     return(
-        <div className='grid-container' id={level}>
+        <div className='grid-container' id="medium">
+            {getGrid(level)}
            {gridChar && gridChar.length>0 && createGrid()}
         </div>
      
