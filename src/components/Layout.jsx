6536fd3147c5fa9f4b14d1c2e649d10b8dc42007
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-11-10 08:21:50 -0800

    make header transparent

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import Header from './Header'
-import { Outlet } from 'react-router-dom'
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
-    return(
-        <>
-            <Header />
-            <Outlet />
-        </>
-    )
+  return (
+    <div className="layout-container min-h screen">
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
