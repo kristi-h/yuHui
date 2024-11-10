df19879000a42ec1aefa0d88f9064a585aa5d684
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-11-09 23:51:19 -0800

    add background image for homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
- export default function Header(){
-    return(
-        <header>
-            <Link className='title' to='/'>YuHui</Link>
-        </header>
-    )
- }
+export default function Header() {
+  return (
+    <header>
+      <Link className="header" to="/">
+        YuHui
+      </Link>
+    </header>
+  );
+}
