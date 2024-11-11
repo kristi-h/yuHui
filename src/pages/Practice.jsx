72fddfb3085bf74738cb43b11bfb107c9abe69ed
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-11-10 22:42:56 -0800

    adjust home-container andgrid-container width, margins

--- src/pages/Practice.jsx
+++ src/pages/Practice.jsx
@@ -6,7 +6,6 @@ import { useLocation } from "react-router-dom";
 import { shuffle } from "./Home";
 
 export default function Practice() {
-  // State and Context
   const { state } = useLocation();
   const { level } = state;
   const [cluster, setCluster] = useState(state.cluster);
@@ -15,7 +14,6 @@ export default function Practice() {
   const [questionBank, setQuestionBank] = useState(cluster);
   const [gameOver, setGameOver] = useState(false);
 
-  // Shuffle on first load
   useEffect(() => {
     shuffle(cluster);
   }, [cluster, questionWord]);
@@ -57,16 +55,15 @@ export default function Practice() {
   }
 
   return (
-    <div className="practice-container flex flex-col items-center bg-gradient-to-b from-[#f8f3e7] to-[#e2d7c3] min-h-screen py-10">
-      <h3 className="text-[#8b4513] font-semibold text-2xl mb-4">
+    <div className="practice-container">
+      <h3 className="question_word">
         English Word:{" "}
         <span className="font-serif text-[#7a3e19]">
           {questionWord.English}
         </span>
       </h3>
-      <h3 className="text-[#8b4513] font-semibold text-2xl mb-4">
-        Pinyin:{" "}
-        <span className="font-serif text-[#7a3e19]">{questionWord.Pinyin}</span>
+      <h3 className="question_word">
+        Pinyin: <span>{questionWord.Pinyin}</span>
       </h3>
 
       <div className="mb-8">
@@ -74,16 +71,10 @@ export default function Practice() {
       </div>
 
       <div className="flex justify-around">
-        <button
-          className="btn bg-[#8b4513] text-[#fef6e4] font-bold py-2 px-4 rounded-lg shadow-md transition-transform transform hover:scale-105 hover:bg-[#6a3c1d]"
-          onClick={getNextWord}
-        >
+        <button className="previous" onClick={getNextWord}>
           Previous
         </button>
-        <button
-          className="btn bg-[#8b4513] text-[#fef6e4] font-bold py-2 px-4 rounded-lg shadow-md transition-transform transform hover:scale-105 hover:bg-[#6a3c1d]"
-          onClick={getNextWord}
-        >
+        <button className="next" onClick={getNextWord}>
           Next
         </button>
       </div>
