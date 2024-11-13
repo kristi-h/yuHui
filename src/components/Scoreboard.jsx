2a517f6bf5197fa2eefdf85e208670bb153256f5
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-11-12 23:24:05 -0800

    add ternary for empty incorrect array

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -2,16 +2,22 @@ export default function Scoreboard({ incorrect }) {
   return (
     <div className="scoreboard">
       <h1>Scoreboard</h1>
-      <h4>Words you got incorrect: </h4>
-      <div className="incorrect-table">
-        {incorrect.map((word, index) => (
-          <div key={index} className="incorrect-table-item">
-            <p>
-              {word.Chinese} - {word.Pinyin} - {word.English}
-            </p>
+      {!incorrect || incorrect.length === 0 ? (
+        <h3>You had no mistakes, way to go!!!</h3>
+      ) : (
+        <div>
+          <h3>Incorrect words:</h3>
+          <div className="incorrect-table">
+            {incorrect.map((word, index) => (
+              <div key={index} className="incorrect-table-item">
+                <p>
+                  {word.Chinese} - {word.Pinyin} - {word.English}
+                </p>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
