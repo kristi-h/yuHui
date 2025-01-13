d1d1c1814682370063bf06b7533d5f6cb982bf22
Kristi Hwang <kristi.y.l.h@gmail.com>
2025-01-13 12:02:12 -0800

    contain firefly randomization to Firefly component's useEffect for better encapsulation and reusability, replace hardcoded inline styles with CSS custom properties, make randomization more consistent by handling once component has mounted

--- src/components/fireflies/Firefly.jsx
+++ src/components/fireflies/Firefly.jsx
@@ -1,31 +1,31 @@
+import { useEffect, useState } from "react";
 import "./firefly.css";
 
-const Firefly = () => {
-  const colors = ["#FFFF33", "#FFC700", "#4EF2F0", "#8FFBFF"];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  const randomSize = `${Math.random() * 5 + 5}px`;
-  const randomPosition = () => ({
-    top: `${Math.random() * 100}vh`,
-    left: `${Math.random() * 100}vw`,
-    animationDelay: `${Math.random() * 5}s`,
-  });
+const Firefly = ({ index }) => {
+  const [styles, setStyles] = useState({});
 
-  const randomBlinkDelay = {
-    animationDelay: `${Math.random() * 2}s`,
-    animationDuration: `${1.5 + Math.random() * 2}s`,
-  };
+  useEffect(() => {
+    const randomPosition = {
+      top: `${Math.random() * 100}vh`,
+      left: `${Math.random() * 100}vw`,
+    };
 
-  return (
-    <div
-      className="firefly"
-      style={{ ...randomPosition(), width: randomSize, height: randomSize }}
-    >
-      <div
-        className="glow"
-        style={{ ...randomBlinkDelay, backgroundColor: randomColor }}
-      ></div>
-    </div>
-  );
+    const randomSize = `${Math.random() * 5 + 5}px`;
+    const randomFloatDuration = `${4 + Math.random() * 2}s`; // 4-6s
+    const randomGlowDuration = `${1.5 + Math.random() * 1.5}s`; // 1.5-3s
+    const randomGlowDelay = `${Math.random() * 2}s`; // 0-2s
+
+    setStyles({
+      ...randomPosition,
+      width: randomSize,
+      height: randomSize,
+      animation: `float ${randomFloatDuration} infinite ease-in-out`,
+      "--glow-duration": randomGlowDuration,
+      "--glow-delay": randomGlowDelay,
+    });
+  }, [index]);
+
+  return <div className="firefly" style={styles}></div>;
 };
 
 export default Firefly;
