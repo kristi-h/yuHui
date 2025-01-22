dea48e07d4e304b49d02b229311edc4ec31a0743
Kristi Hwang <kristi.y.l.h@gmail.com>
2025-01-21 23:36:50 -0800

    add DarkModeContext

---
+++ src/contexts/DarkModeContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+export const DarkModeContext = createContext();
+
+export const DarkModeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+    document.body.className = isDarkMode ? "light-mode" : "dark-mode";
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
